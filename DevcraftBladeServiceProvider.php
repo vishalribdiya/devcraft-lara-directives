86d483a6cb6113c43e282df16ec9c4b19d3aaddc
Vishal Ribdiya <70788916+vishalribdiya@users.noreply.github.com>
2020-09-13 14:40:22 +0530

    Create DevcraftBladeServiceProvider.php

---
+++ DevcraftBladeServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DevCraft\Directives;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class DevcraftBladeServiceProvider
+ */
+class DevcraftBladeServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->registerDirectives();
+
+    }
+
+    /**
+     * Register all directives.
+     *
+     * @return void
+     */
+    public function registerDirectives()
+    {
+        $directives = require __DIR__.'/directives.php';
+
+        collect($directives)->each(function ($item, $key) {
+            Blade::directive($key, $item);
+        });
+    }
+
+}
